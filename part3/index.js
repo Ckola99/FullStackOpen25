36e8df7797b2d4446a5c5ee3a40ced1de395eb18
Christopher kola <Christopherkola@gmail.com>
2025-12-13 15:12:38 +0200

    REFACTOR: Refactored to use cors and dynamically accept requests to backend

---
+++ part3/index.js
@@ -0,0 +1,132 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+const allowedOrigins = [
+	'http://localhost:5173',
+	'https://YOUR_VERCEL_FRONTEND_URL' // <-- Replace this placeholder later!
+];
+
+const corsOptions = {
+	origin: function (origin, callback) {
+		// allow requests with no origin (like mobile apps or curl)
+		if (!origin) return callback(null, true);
+		if (allowedOrigins.indexOf(origin) === -1) {
+			const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+			return callback(new Error(msg), false);
+		}
+		return callback(null, true);
+	}
+};
+
+
+// custom token to log the request body
+morgan.token('body', (req) => JSON.stringify(req.body))
+const morganFormat = ':method :url :status :res[content-length] - :response-time ms :body'
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(morgan(morganFormat))
+
+
+let persons = [
+	{
+		id: 1,
+		name: "Arto Hellas",
+		number: "040-123456"
+	},
+	{
+		id: 2,
+		name: "Ada Lovelace",
+		number: "39-44-5323523"
+	},
+	{
+		id: 3,
+		name: "Dan Abramov",
+		number: "12-43-234345"
+	},
+	{
+		id: 4,
+		name: "Mary Poppendieck",
+		number: "39-23-6423122"
+	}
+]
+
+app.get('/', (req, res) => {
+	res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+	const date = new Date()
+	res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
+app.get('/api/persons', (req, res) => {
+	res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find(person => person.id === id)
+
+	if (person) {
+		res.json(person)
+	} else {
+		res.status(404).end() //end method is used to indicate that the request should be responded to without data.
+	}
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	persons = persons.filter(person => person.id !== id)
+
+	res.status(204).end()
+})
+
+const generateId = () => {
+	const maxId = persons.length > 0
+		? Math.max(...persons.map(person => person.id))
+		: 0
+	return maxId + 1
+}
+
+app.post('/api/persons', (req, res) => {
+	const body = req.body
+
+	if (!body.name) {
+		return res.status(400).json({
+			error: 'name missing'
+		})
+	}
+
+	if (!body.number) {
+		return res.status(400).json({
+			error: 'number missing'
+		})
+	}
+
+	const nameExists = persons.find(person => person.name === body.name)
+	if (nameExists) {
+		return res.status(400).json({
+			error: 'name must be unique'
+		})
+	}
+
+	const newPerson = {
+		id: generateId(),
+		name: body.name,
+		number: body.number
+	}
+
+	persons = persons.concat(newPerson)
+
+	res.json(newPerson)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
